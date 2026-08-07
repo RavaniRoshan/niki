b635075f1765e70ca6cf2943c758d0b24a18e45b
RavaniRoshan <shivamkumar10958@gmail.com>
2026-08-07 04:51:40 +0000

    Add package manager manifests: Homebrew, winget, Scoop
    - homebrew/niki.rb: Homebrew formula with platform-specific SHA256 checksums
    - winget/: Windows Package Manager manifests (installer, locale, version)
    - scoop/niki.json: Scoop bucket manifest with autoupdate support
    
    These enable: brew install niki, winget install niki, scoop install niki

---
+++ homebrew/niki.rb
@@ -0,0 +1,35 @@
+class Niki < Formula
+  desc "Hermetic multi-agent coding system"
+  homepage "https://github.com/RavaniRoshan/niki"
+  license "BUSL-1.1"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/RavaniRoshan/niki/releases/download/v0.2.0/niki-aarch64-apple-darwin.tar.xz"
+      sha256 "cbe3e56070ed9d245f9b25ec851a5e2c86cf7a2ff522c9c2dd8d10fe9cb0bc58"
+    end
+    on_intel do
+      url "https://github.com/RavaniRoshan/niki/releases/download/v0.2.0/niki-x86_64-apple-darwin.tar.xz"
+      sha256 "36d281d412e25c836e842cf5293c01237d7f0f5ae239e2e55abd29b161bf5b88"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/RavaniRoshan/niki/releases/download/v0.2.0/niki-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "277afd6d8b90254514ff69479c2b3fb163f8a5cc7c255ccce62a65b9c0b0b19e"
+    end
+    on_intel do
+      url "https://github.com/RavaniRoshan/niki/releases/download/v0.2.0/niki-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "c1e2e4c6cc24e2d0f381f83aafb55b9dfde13c4b050ca7cee8d68f44124a09ca"
+    end
+  end
+
+  def install
+    bin.install "niki"
+  end
+
+  test do
+    assert_match "niki #{version}", shell_output("#{bin}/niki --version")
+  end
+end
